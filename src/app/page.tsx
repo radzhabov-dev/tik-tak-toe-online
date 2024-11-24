79632a6332c0b11b6c5befdae5af76546db9af0e
rustam18-dev <rustam.radzabov8410@gmail.com>
2024-11-24 16:13:14 +0500

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import {Button} from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
+import { prisma } from "@/shared/lib/db";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
   return (
     <div>
       <Button>Hello</Button>
+
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
